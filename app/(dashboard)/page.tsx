6ca8eb57c04746a4bbdb5a31cb8131e66d326dc3
alizcelik <ali.ozcelik_01@metu.edu.tr>
2024-06-02 15:50:53 +0300

    initial authentication

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p>
+        This is an authenticated page. You can only see this if you are signed in.
+      </p>
+    </div>
+  );
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TestPage() {
+  return (
+    <div>
+      <p>
+        This is an authenticated page. You can only see this if you are signed in.
+      </p>
+    </div>
+  )
+}
+
+export default TestPage
